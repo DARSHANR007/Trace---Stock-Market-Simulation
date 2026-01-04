2fbf8ffac56906605d12c31c42248839aa6fce40
darsh <darshanravichandran07@gmail.com>
2026-01-04 23:26:40 +0530

    Add OAuth login flow and .gitignore for .env
    Introduces a basic OAuth login flow using Upstox API with /login and callback handlers in main.go. Also adds a .gitignore file to exclude .env from version control.

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+var (
+	clientID    = os.Getenv("APKEY")
+	redirectURI = os.Getenv("REDIRECT_URI")
+)
 
 func main() {
-	fmt.Println("stocks")
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/", callbackHandler)
+
+	fmt.Println("Server running at http://localhost:8000")
+	fmt.Println("Open http://localhost:8000/login")
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	authURL := "https://api.upstox.com/v2/login/authorization/dialog"
+
+	params := url.Values{}
+	params.Add("response_type", "code")
+	params.Add("client_id", clientID)
+	params.Add("redirect_uri", redirectURI)
+	params.Add("state", "dev_state_123")
+
+	finalURL := authURL + "?" + params.Encode()
+	http.Redirect(w, r, finalURL, http.StatusFound)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+
+	if code == "" {
+		fmt.Fprintln(w, "Go to /login to authenticate")
+		return
+	}
+
+	fmt.Println("Authorization code:", code)
+	fmt.Fprintln(w, "Login successful. Check terminal.")
 }
