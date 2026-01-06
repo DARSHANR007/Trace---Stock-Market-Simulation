8ac45da0f0927b98125801a7e6443ddd02450a88
darsh <darshanravichandran07@gmail.com>
2026-01-06 11:51:39 +0530

    Update getinstrumentby_symbol.go

--- internal/market/getinstrumentby_symbol.go
+++ internal/market/getinstrumentby_symbol.go
@@ -10,16 +10,13 @@ func GetInstrumentKeyBySymbol(db *sql.DB, symbol string) (string, error) {
 	query := `
 		SELECT instrument_key
 		FROM instruments
-		WHERE (
-			UPPER(name) = UPPER(?)
-			OR trading_symbol = ? || '-EQ'
-		)
-		AND segment IN ('NSE_EQ', 'BSE_EQ')
+		WHERE trading_symbol = ?
+		  AND segment IN ('NSE_EQ', 'BSE_EQ')
 		ORDER BY
-			CASE
-				WHEN segment = 'NSE_EQ' THEN 1
-				WHEN segment = 'BSE_EQ' THEN 2
-			END
+		  CASE
+			WHEN segment = 'NSE_EQ' THEN 1
+			WHEN segment = 'BSE_EQ' THEN 2
+		  END
 		LIMIT 1;
 	`
 	var instrumentKey string
